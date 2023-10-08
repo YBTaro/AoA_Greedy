667b01d6d5887fe33f8d43c48320d7fb9673b6ce
Maxim <jacky85019@gmail.com>
2023-10-08 13:23:17 -0400

    Add comment and fix

--- experiment.py
+++ experiment.py
@@ -9,17 +9,20 @@ from run3 import strat3
 from run4 import strat4
 
 def experiment(n_array, array, k_array): # give the number of total devices and working devices, return the result of using four greedy algos
-    experiment_result = {k: [[] for _ in range(4)] for k in k_array}
+    experiment_result = {k: [[] for _ in range(4)] for k in k_array} # dictionary {k1:[[],[],[],[]], k2:[[],[],[],[]],k3:[[],[],[],[]],}
+    # print(experiment_result)
     for k, result in experiment_result.items():
-        for n in n_array:
-            results = [
-                strat1(n, k, array[n], False),
+        for n in n_array: # do len(n_array) times
+            arr1 = array[n]
+            results = [ # problem : does all the strat use the same array?
+                strat1(n, k, array[n], False), # ****array is a dictionary**** array[n] is the randomly created array with exactly n bags
                 strat2(n, k, array[n], False),
                 strat3(n, k, array[n], False),
                 strat4(n, k, array[n], False)
             ]
-            for i, arr in enumerate(result):
+            for i, arr in enumerate(result): # result = [[],[],[],[]] arr = [n等於1000,2000, 3000時，計算的結果]
                 arr.append(results[i])
+        print(result)
     return experiment_result
 
 def plot_result(n_array, result, k):
@@ -40,10 +43,10 @@ def plot_result(n_array, result, k):
     # plt.show()
 
 def main():
-    n_array = [1000, 2000, 3000, 4000, 5000]
-    k_array = [100, 500, 800, 1000]
+    n_array = [5]
+    k_array = [5]
 
-    array = {n: [] for n in n_array}
+    array = {n: [] for n in n_array} # dictionary {n1:[],n2:[],n3:[]} This is a dictionary named "array" lol
     for n, arr in array.items():
         for _ in range(n): # randomly create array with n total devices
             total = random.randint(0, 9999)

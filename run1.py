cac6e7911ff9c51b0c74b515c61d98383796f51d
Jat Li <janet881005@gmail.com>
2023-10-09 00:10:57 -0400

    refactor: add comments

--- run1.py
+++ run1.py
@@ -3,29 +3,33 @@ from queue import PriorityQueue
 def strat1(n, k, array, print_result=True):
     queue = PriorityQueue()
     for i, arr in enumerate(array):
-        queue.put((arr[0], i, arr))
+        queue.put((arr[0], i, arr)) # put element according to numWorking
 
-    for i in range(0, k):
-        _, index, bag = queue.get()
-        bag[0] += 1
-        bag[1] += 1
-        queue.put((bag[0], index, bag))
-        if print_result:
-            print(index, end=" ")
-    
+    result = ""
+    for i in range(0, k):   # put k devices one by one
+        _, index, bag = queue.get() # selected one is out of queue
+        bag[0] += 1 # numWorking
+        bag[1] += 1 # total
+        queue.put((bag[0], index, bag)) # change values and put it back to queue
+        result += "%d "%index
+
+    if print_result:
+        print(result)
+
     percentage = 0
     while (not queue.empty()):
         _, _, bag = queue.get()
         percentage += bag[0]/bag[1]
     
-    return round(100*percentage/n, 2)
+    return round(100*percentage/n, 2)   # return final percentage
 
 if __name__ == "__main__":
-    n, k = input().split()
-    n, k = int(n), int(k)
+    n, k = input().split()  # input reader
+    n, k = int(n), int(k)   # type casting
     array = []
     for i in range(n):
         numWork, total = input().split()
+        # 2d-array, 1st column is numWorking, 2nd is total
         array.append([int(numWork), int(total)])
 
     strat1(n, k, array)
--- run4.py
+++ run4.py
@@ -1,35 +1,40 @@
 from queue import PriorityQueue
 
+# calculate priority value
 def priority_calculate(numWork, total):
     return (numWork+1)/(total+1)-numWork/total
 
 def strat4(n, k, array, print_result=True):
     queue = PriorityQueue()
     for i, arr in enumerate(array):
-        # want the max, so add negative sign to priority_calculate
+        # want the max, so add negative sign to priority value
         queue.put((-priority_calculate(arr[0], arr[1]), i, arr))
 
-    for i in range(0, k):
-        _, index, bag = queue.get()
-        bag[0] += 1
-        bag[1] += 1
-        queue.put((-priority_calculate(bag[0], bag[1]), index, bag))
-        if print_result:
-            print(index, end=" ")
+    result = ""
+    for i in range(0, k):   # put k devices one by one
+        _, index, bag = queue.get() # selected one is out of queue
+        bag[0] += 1 # numWorking
+        bag[1] += 1 # total
+        queue.put((-priority_calculate(bag[0], bag[1]), index, bag)) # change values and put it back to queue
+        result += "%d "%index
+    
+    if print_result:
+        print(result)
 
     percentage = 0
     while (not queue.empty()):
         _, _, bag = queue.get()
         percentage += bag[0]/bag[1]
     
-    return round(100*percentage/n, 2)
+    return round(100*percentage/n, 2)   # return final percentage
 
 if __name__ == "__main__":
-    n, k = input().split()
-    n, k = int(n), int(k)
+    n, k = input().split()  # input reader
+    n, k = int(n), int(k)   # type casting
     array = []
     for i in range(n):
         numWork, total = input().split()
+        # 2d-array, 1st column is numWorking, 2nd is total
         array.append([int(numWork), int(total)])
 
     strat4(n, k, array)
